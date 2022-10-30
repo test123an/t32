e98338c9e94c9615d533712c2b758282705ddddc
test123an <117013125+test123an@users.noreply.github.com>
2022-10-30 17:02:28 +0600

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,26 @@
+import math
+f = open("i.txt", "r")
+lines = f.read().split(' ')
+nums = list(map(int, lines))
+
+def _min(a):
+    m=min(a)
+    return m
+
+def _max(a):
+    m=max(a)
+    return m
+
+def _sum(a):
+    m=sum(a)
+    return m
+
+def _mult(a):
+    m=math.prod(a)
+    return m
+
+print ("В файле: ", *nums, sep=" ")
+print ("Минимальное: ", _min(nums))
+print ("Максимальное: ",_max(nums))
+print ("Сумма: ",_sum(nums))
+print ("Произведение: ",_mult(nums))
